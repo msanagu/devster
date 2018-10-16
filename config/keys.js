95fb5e1a118dd1eb99ddb4baf99cf94bc750c51e
msanagu <marycsanagustin@gmail.com>
2018-10-16 10:41:21 -0700

    JTW token is generated for login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://msanagu:04161984Saf!@ds045557.mlab.com:45557/devster"
+  mongoURI: "mongodb://msanagu:04161984Saf!@ds045557.mlab.com:45557/devster",
+  secretOrKey: "secret"
 };
